5aa7d07a8e4fb73d9e935a05cce895f4b2b54c5e
Chris Higgins <chris.higgins@alternoc.net>
2024-08-02 12:23:26 -0700

    reformatting for readability

--- ccregex.py
+++ ccregex.py
@@ -46,6 +46,7 @@ def find_cards(args, input_line, linenum, input_filename):
         card_type = "Visa/MasterCard/American Express/Discover"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(
         r"(?:3[47]\d{2}([ \-]?)\d{6}\d|(?:(?:4\d|5[1-5]|65)\d{2}|6011)([ \-]?)\d{4}\d{4})\d{4}",
         input_line,
@@ -54,6 +55,7 @@ def find_cards(args, input_line, linenum, input_filename):
         card_type = "credit/debit"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(
         r"(?ms)(.*)\b(?:4[0-9]{8}(?:[0-9]{3})?|5[1-5][0-9]{10}|6(?:011|5[0-9]{2})[0-9]{8}|3[47][0-9]{9}|3(?:0[0-5]|[68][0-9])[0-9]{7}|(?:2131|1800|35\d{3})\d{7})(\d{4}\b.*)",
         input_line,
@@ -62,51 +64,61 @@ def find_cards(args, input_line, linenum, input_filename):
         card_type = "credit/debit"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b3[47][0-9]{13}\b.", input_line)
     if found_pattern is not None:
         card_type = "American Express (AMEX)"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b(6541|6556)[0-9]{12}\b.", input_line)
     if found_pattern is not None:
         card_type = "BCGlobal"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b389[0-9]{11}\b.", input_line)
     if found_pattern is not None:
         card_type = "Carte Blanche"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b3(?:0[0-5]|[68][0-9])[0-9]{11}\b.", input_line)
     if found_pattern is not None:
         card_type = "Diners"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b6(?:011|5[0-9]{2})[0-9]{12}\b.", input_line)
     if found_pattern is not None:
         card_type = "Discover"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b63[7-9][0-9]{13}\b.", input_line)
     if found_pattern is not None:
         card_type = "Insta Paymentcard"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b(?:2131|1800|35\d{3})\d{11}\b.", input_line)
     if found_pattern is not None:
         card_type = "JCB"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b9[0-9]{15}\b.", input_line)
     if found_pattern is not None:
         card_type = "Korean Local card"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b(6304|6706|6709|6771)[0-9]{12,15}\b.", input_line)
     if found_pattern is not None:
         card_type = "LaserCard"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(
         r"\b(5018|5020|5038|6304|6759|6761|6763)[0-9]{8,15}\b.", input_line
     )
@@ -114,11 +126,13 @@ def find_cards(args, input_line, linenum, input_filename):
         card_type = "Maestro"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b5[1-5][0-9]{14}\b.", input_line)
     if found_pattern is not None:
         card_type = "Mastercard"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"5[1-5][0-9]{14}", input_line)
     if found_pattern is not None:
         card_type = "SoloCard"
@@ -133,6 +147,7 @@ def find_cards(args, input_line, linenum, input_filename):
         card_type = "SwitchCard"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(
         r"\b(4903|4905|4911|4936|6333|6759)[0-9]{12}|(4903|4905|4911|4936|6333|6759)[0-9]{14}|(4903|4905|4911|4936|6333|6759)[0-9]{15}|564182[0-9]{10}|564182[0-9]{12}|564182[0-9]{13}|633110[0-9]{10}|633110[0-9]{12}|633110[0-9]{13}\b.",
         input_line,
@@ -141,11 +156,13 @@ def find_cards(args, input_line, linenum, input_filename):
         card_type = "credit/debit"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b4[0-9]{12}(?:[0-9]{3})?\b.", input_line)
     if found_pattern is not None:
         card_type = "credit/debit"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(
         r"\b(?:3[47]\d{2}([ \-]?)\d{6}\b\d|(?:(?:4\d|5[1-5]|65)\d{2}|6011)([ \-]?)\d{4}\b\d{4}\b)\d{4}\b",
         input_line,
@@ -154,6 +171,7 @@ def find_cards(args, input_line, linenum, input_filename):
         card_type = "credit/debit"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(
         r"(?:3[47]\d{2}([ \-]?)\d{6}\b\d|(?:(?:4\d|5[1-5]|65)\d{2}|6011)([ \-]?)\d{4}\b\d{4}\b)\d{4}",
         input_line,
@@ -162,26 +180,31 @@ def find_cards(args, input_line, linenum, input_filename):
         card_type = "credit/debit"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b(62[0-9]{14,17})\b.", input_line)
     if found_pattern is not None:
         card_type = "ICARD"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"\b(4[0-9]{14,17})\b.", input_line)
     if found_pattern is not None:
         card_type = "Visa Mastercard"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"(4[0-9]{14,17})", input_line)
     if found_pattern is not None:
         card_type = "Visa Mastercard"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"((?<!\.)4[0-9]{14,17})", input_line)
     if found_pattern is not None:
         card_type = "credit/debit"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(
         r"\b(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})\b.", input_line
     )
@@ -189,16 +212,19 @@ def find_cards(args, input_line, linenum, input_filename):
         card_type = "Visa Mastercard"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"^4[0-9]{12}(?:[0-9]{3})?$", input_line)
     if found_pattern is not None:
         card_type = "Visa"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"(0200[0-9]{43})(4[0-9]{12}[0-9]{3})", input_line)
     if found_pattern is not None:
         card_type = "credit/debit"
         found_card(args, input_line, card_type, found_pattern, linenum, input_filename)
         return 1
+
     found_pattern = re.search(r"^4\d{3}([ \-]?)\d{4}\d{4}\d{4}$", input_line)
     if found_pattern is not None:
         card_type = "credit/debit"
